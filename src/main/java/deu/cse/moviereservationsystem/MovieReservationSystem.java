295ccb00c00b4c29255d7250cb9c6e34f243a08a
Bae Jae Jin <crystal10806@naver.com>
2024-05-16 20:46:13 +0900

    feat : 패키지 리팩토링 (#7)

---
+++ src/main/java/deu/cse/moviereservationsystem/MovieReservationSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package deu.cse.moviereservationsystem;
+
+import deu.cse.moviereservationsystem.view.MainFrame;
+import deu.cse.moviereservationsystem.view.User.LoginFrame;
+
+/**
+ *
+ * @author LG
+ */
+public class MovieReservationSystem {
+
+    public static void main(String[] args) {
+        MainFrame main = new MainFrame();
+        main.setVisible(true);
+    }
+}
