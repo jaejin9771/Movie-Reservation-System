8fb5269e1e0d2415f1121e7809addd62561884eb
Bae Jae Jin <crystal10806@naver.com>
2024-05-16 16:12:12 +0900

    feat : GUI 초기 세팅(#1) (#2)

---
+++ src/main/java/com/mycompany/moviereservationsystem/MovieReservationSystem.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.moviereservationsystem;
+
+import com.mycompany.moviereservationsystem.view.User.LoginFrame;
+
+/**
+ *
+ * @author LG
+ */
+public class MovieReservationSystem {
+
+    public static void main(String[] args) {
+        LoginFrame login = new LoginFrame();
+        login.setVisible(true);
+    }
+}
