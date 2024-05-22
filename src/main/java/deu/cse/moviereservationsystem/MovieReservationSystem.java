de3d284a52869975eb6e71a095fd03a6dc28cf5c
JiMinL03 <wlalswwkd0@naver.com>
2024-05-23 02:05:57 +0900

    feat : sweetShop 파일출력 split 구현 (#25)

--- src/main/java/deu/cse/moviereservationsystem/MovieReservationSystem.java
+++ src/main/java/deu/cse/moviereservationsystem/MovieReservationSystem.java
@@ -4,7 +4,6 @@
 
 package deu.cse.moviereservationsystem;
 
-import deu.cse.moviereservationsystem.Controller.SweetShop.SweetShopController;
 import deu.cse.moviereservationsystem.view.MainFrame;
 
 /**
@@ -16,8 +15,5 @@ public class MovieReservationSystem {
     public static void main(String[] args) {
         MainFrame main = new MainFrame();
         main.setVisible(true);
-        
-        SweetShopController c = new SweetShopController();
-        //c.separateOrder(c.pickOrder("이지민"));
     }
 }
